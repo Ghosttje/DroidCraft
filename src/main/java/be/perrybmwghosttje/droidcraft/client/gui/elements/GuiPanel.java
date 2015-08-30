0a9077c09547ab1d258daf4413001e6f133896fd
Ghosttje <kevineke@gmail.com>
2015-08-30 21:54:41 +0200

    - Created class for gui panel

---
+++ src/main/java/be/perrybmwghosttje/droidcraft/client/gui/elements/GuiPanel.java
@@ -0,0 +1,14 @@
+package be.perrybmwghosttje.droidcraft.client.gui.elements;
+
+import be.perrybmwghosttje.droidcraft.client.gui.elements.base.GuiElementBase;
+
+/**
+ * Created by kevin on 30-Aug-15.
+ */
+public class GuiPanel extends GuiElementBase {
+
+    @Override
+    public void drawBackGround(float delta, int x, int y) {
+
+    }
+}
